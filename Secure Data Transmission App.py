af1ef1ac6febb88adb280a342035089a7943d1d0
HugueAX <rvnchsn@gmail.com>
2026-09-19 02:05:39 -0400

    Update Secure Data Transmission App.py

--- Secure Data Transmission App.py
+++ Secure Data Transmission App.py
@@ -25,21 +25,33 @@ def decrypt_message(encrypted_message, cipher):
     decrypted_message = decrypted_bytes.decode('utf-8')
     return decrypted_message
 
-def compare_hashes(hashed_message, decrypted_hashed_message):
+def compare_hashes(hashed_message, hashed_decrypted_message):
     if (hashed_message == hashed_decrypted_message):
         print("\n[SUCCESS] Message security verified by hash comparison.")
     else:
         print("\n[ERROR] Message failed verification.")
 
+# Gets user input
 message = input("Enter message to encrypt: ")
-hashed_message = hash_message(message)
-cipher, encrypted_message = encrypt_message(message)
-decrypted_message = decrypt_message(encrypted_message, cipher)
-hashed_decrypted_message = hash_message(decrypted_message)
-compare_hashes(hashed_message, hashed_decrypted_message)
 
+# Hashes message
+hashed_message = hash_message(message)
+
+# Encrpts message
+cipher, encrypted_message = encrypt_message(message)
+
+# Decrypts message
+decrypted_message = decrypt_message(encrypted_message, cipher)
+
+# Hashes decrypted message to prepare for comparison
+hashed_decrypted_message = hash_message(decrypted_message)
+
+# Prints messages and hashes to review 
 print(f"\nOriginal Message:          {message}")
 print(f"\nOriginal Hash:             {hashed_message}")
 print(f"\nEncrypted Message:         {encrypted_message}")
 print(f"\nDecrypted Message:         {decrypted_message}")
 print(f"\nDecryted Hash:             {hashed_decrypted_message}")
+
+# Compares hashes for security
+compare_hashes(hashed_message, hashed_decrypted_message)
