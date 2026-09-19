457e76e7c1c8dcd6a843df66952aa92954e49280
HugueAX <rvnchsn@gmail.com>
2026-09-19 01:55:35 -0400

    Update Secure Data Transmission App.py

--- Secure Data Transmission App.py
+++ Secure Data Transmission App.py
@@ -1,12 +1,12 @@
 from cryptography.fernet import Fernet
 import hashlib
 
-# convert user string to hash
-def hash_string(user_string):
+# Convert message to hash
+def hash_message(user_string):
     return hashlib.sha256(user_string.encode('utf-8')).hexdigest()
-    
 
-def encryptHash(hashed_message):
+# Encrpt message
+def encrypt_message(message):
     # Generate key
     key = Fernet.generate_key()
 
@@ -14,30 +14,32 @@ def encryptHash(hashed_message):
     cipher = Fernet(key)
 
     # Encrypt message
-    message_bytes = hashed_message.encode("utf-8")
+    message_bytes = message.encode("utf-8")
     cipher_text = cipher.encrypt(message_bytes)
-    # Return cipher and encrypted text
     return cipher, cipher_text
 
-def decryptHash(encrypted_hashed_message, cipher):
+# Decrypt message
+def decrypt_message(encrypted_message, cipher):
     # Decrypt the message
-    decrypted_bytes = cipher.decrypt(encrypted_hashed_message)
+    decrypted_bytes = cipher.decrypt(encrypted_message)
     decrypted_message = decrypted_bytes.decode('utf-8')
     return decrypted_message
 
-def compareHashes(hashed_message, decrypted_hashed_message):
-    if (hashed_message == decrypted_hashed_message):
+def compare_hashes(hashed_message, decrypted_hashed_message):
+    if (hashed_message == hashed_decrypted_message):
         print("\n[SUCCESS] Message security verified by hash comparison.")
     else:
         print("\n[ERROR] Message failed verification.")
 
 message = input("Enter message to encrypt: ")
-hashed_message = hash_string(message)
-cipher, encrypted_hashed_message = encryptHash(hashed_message)
-decrypted_hashed_message = decryptHash(encrypted_hashed_message, cipher)
-compareHashes(hashed_message, decrypted_hashed_message)
+hashed_message = hash_message(message)
+cipher, encrypted_message = encrypt_message(message)
+decrypted_message = decrypt_message(encrypted_message, cipher)
+hashed_decrypted_message = hash_message(decrypted_message)
+compare_hashes(hashed_message, hashed_decrypted_message)
 
-print(f"Original Message:          {message}")
-print(f"Original Hash:             {hashed_message}")
-print(f"Encrypted Hash:    {encrypted_hashed_message}")
-print(f"Decrypted Hash:            {decrypted_hashed_message}")
+print(f"\nOriginal Message:          {message}")
+print(f"\nOriginal Hash:             {hashed_message}")
+print(f"\nEncrypted Message:         {encrypted_message}")
+print(f"\nDecrypted Message:         {decrypted_message}")
+print(f"\nDecryted Hash:             {hashed_decrypted_message}")
