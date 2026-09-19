d6660142cba6c79e66999725e0a9b32ab1dc4dfe
HugueAX <rvnchsn@gmail.com>
2026-09-19 01:20:35 -0400

    Update and rename Secure Data Transmission App to Secure Data Transmission App.py

--- Secure Data Transmission App.py
+++ Secure Data Transmission App.py
@@ -1,6 +1,5 @@
 from cryptography.fernet import Fernet
 import hashlib
-import sys
 
 # convert user string to hash
 def hash_string(user_string):
@@ -17,27 +16,28 @@ def encryptHash(hashed_message):
     # Encrypt message
     message_bytes = hashed_message.encode("utf-8")
     cipher_text = cipher.encrypt(message_bytes)
+    # Return cipher and encrypted text
     return cipher, cipher_text
 
-def decryptHash(encrypted_hashed_message):
+def decryptHash(encrypted_hashed_message, cipher):
     # Decrypt the message
-    decrypted_bytes = cipher.decrypt(cipher_text)
+    decrypted_bytes = cipher.decrypt(encrypted_hashed_message)
     decrypted_message = decrypted_bytes.decode('utf-8')
     return decrypted_message
 
 def compareHashes(hashed_message, decrypted_hashed_message):
-    if (hashed_message === decrypted_hashed_message):
-        print("Message security verified by hash comparison.")
+    if (hashed_message == decrypted_hashed_message):
+        print("\n[SUCCESS] Message security verified by hash comparison.")
     else:
-        print("Message failed verification.")
+        print("\n[ERROR] Message failed verification.")
 
 message = input("Enter message to encrypt: ")
 hashed_message = hash_string(message)
-encrypted_hashed_message = encryptHash(hashed_message)
-decrypted_hashed_message = decryptHash(encrypted_hashed_message)
+cipher, encrypted_hashed_message = encryptHash(hashed_message)
+decrypted_hashed_message = decryptHash(encrypted_hashed_message, cipher)
 compareHashes(hashed_message, decrypted_hashed_message)
 
-print(message)
-print(hashed_message)
-print(encrypted_hashed_message)
-print(decrypted_hashed_message)
+print(f"Original Message:          {message}")
+print(f"Original Hash:             {hashed_message}")
+print(f"Encrypted Hash:    {encrypted_hashed_message}")
+print(f"Decrypted Hash:            {decrypted_hashed_message}")
